aee86fbbbb44d983f15f0929853b8d2ad7726015
nagah <nagah.shinawy@gmail.com>
2023-01-18 12:13:39 +0200

    adding list product url

--- apps/cfe/urls.py
+++ apps/cfe/urls.py
@@ -25,14 +25,25 @@ urlpatterns = [
     path("api/v1/products_list/", products_list, name="products_list"),
     path("api/v1/products/create/", create_product, name="create"),
     path("api/v1/products/<int:pk>/", ProductDetailAPIView.as_view(), name="detail"),
-    path("api/v1/products/update/<int:pk>/", ProductUpdateAPIView.as_view(), name="update"),
-    path("api/v1/products/delete/<int:pk>/", ProductDeleteAPIView.as_view(), name="delete"),
+    path(
+        "api/v1/products/update/<int:pk>/",
+        ProductUpdateAPIView.as_view(),
+        name="update",
+    ),
+    path(
+        "api/v1/products/delete/<int:pk>/",
+        ProductDeleteAPIView.as_view(),
+        name="delete",
+    ),
     path("api/v1/products/", ProductListAPIView.as_view(), name="products-list"),
     path("api/v1/products/add/", ProductCreateAPIView.as_view(), name="add"),
-    path("api/v1/products/list-create/<int:pk>/", ProductMixinView.as_view(), name="list-create"),
+    path(
+        "api/v1/products/list-create/<int:pk>/",
+        ProductMixinView.as_view(),
+        name="list-create",
+    ),
     path(
         "api/v1/products/add-list/", ProductListCreateAPIView.as_view(), name="add-list"
-    ),path(
-        "api/v1/products/list/", ProductListAPIView.as_view(), name="list"
     ),
+    path("api/v1/products/list/", ProductListAPIView.as_view(), name="list"),
 ]
